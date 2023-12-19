8836a4d04ce726e7e031d9b64af659a7cbfa2efd
Hein Thu <88365143+hanehein@users.noreply.github.com>
2023-12-19 20:01:58 +0630

    update project section

--- src/component/PojectDetails/ProjectDetailModal.js
+++ src/component/PojectDetails/ProjectDetailModal.js
@@ -1,26 +1,68 @@
-import React from 'react'
+import React from "react";
 import myschool1 from "../../assets/Projectimg/myschool/myschool1.png";
 
-
-const ProjectDetailModal = () => {
-    return (
-      <div className='fixed top-0 left-0 bg-black/90 w-full h-full z-50 flex justify-center items-center overflow-y-scroll'>
-        <div className='w-11/12 h-[90vh] bg-[#424242] p-10'>
-            <div className='w-full'>
-                <div className='w-6/12'>
-                    <img src={myschool1}></img>
-                </div>
-                <div className='w-5/12'>
-                    <div>
-                        <img></img>
-                        <p></p>
-                    </div>
-                </div>
+const ProjectDetailModal = ({ projectData, close }) => {
+  console.log(projectData);
+  return (
+    <div className="fixed top-0 left-0 bg-black/90 w-full h-full z-50 flex justify-center items-center overflow-y-scroll" style={{ fontFamily: 'Lexend' }}>
+      <div className="w-10/12 h-[90vh] bg-[#232323] p-10 overflow-y-auto relative">
+        <div className="w-full flex flex-row items-center space-x-24">
+          <div className="w-7/12 rounded-lg overflow-hidden">
+            <img src={projectData.image[0]}></img>
+          </div>
+          <div className="w-5/12 flex flex-col space-y-3">
+            <div className="flex-row flex items-center space-x-5">
+              <img src={projectData.image[3]} className="w-20"></img>
+              <p className=" text-5xl tracking-wider">{projectData.title}</p>
             </div>
+            <p className=" text-3xl ">{projectData.subTitle}</p>
+            <a href={projectData.website}>
+              <p className="text-[#3A91F8] underline text-xl ">
+                {" "}
+                {projectData.website}
+              </p>
+            </a>
+            <p className="text-xl">{projectData.stack}</p>
+          </div>
         </div>
+
+        <div className="mt-10">
+          <p className="text-xl tracking-wider">{projectData.paragraph}</p>
+        </div>
+
+        <div className="w-full flex flex-row items-center space-x-24 mt-10">
+          
+          <div className="w-6/12">
+            <ul className=" list-disc">
+              {projectData.body1.map((body1) => (
+                <li className="text-xl mb-6">{body1}</li>
+              ))}
+            </ul>
+          </div>
+          <div className=" w-6/12">
+            <img src={projectData.image[1]}></img>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-row items-center space-x-24 mt-10">
+        <div className=" w-6/12">
+            <img src={projectData.image[2]}></img>
+          </div>
+          <div className="w-6/12">
+            <ul className=" list-disc">
+              {projectData.body2.map((body2) => (
+                <li className="text-xl mb-6">{body2}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <button className=" absolute top-6 right-8 " style={{ fontFamily: "Itim" }} onClick={close}>
+          <p className="text-5xl text-red-500">X</p>
+        </button>
       </div>
-    )
-  }
+    </div>
+  );
+};
 
+export default ProjectDetailModal;
 
-export default ProjectDetailModal
--- src/component/Project.js
+++ src/component/Project.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectDetailModal from "./PojectDetails/ProjectDetailModal";
 // image import
 import myschool1 from "../assets/Projectimg/myschool/myschool1.png";
@@ -23,30 +23,36 @@ const projects = [
     title: "My School",
     subTitle: "Student Management Software",
     website: "http://www.exbrainedu.com",
-    languages: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    stack: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    languages: ["Vue", "Inertia", "Laravel", "Tailwind"],
     paragraph:
       "My School is a comprehensive online platform designed to streamline various administrative tasks and enhance communication between students, teachers, and administrators. This documentation provides a brief overview of the key features and functionalities of the website",
     image: [myschool1, myschool2, myschool3, myschoollogo],
-    body: [
+    body1: [
       "Student Management: Manage student profiles and information.",
       "Monthly Report Attendance & Exam Mark: Generate and manage monthly reports, attendance records, and exam marks.",
       "Admin Roles: Assign different access levels and permissions to administrators.",
+    ],
+    body2: [
       "Lecture and Video Upload: Easily upload and stream lectures and videos.",
       "User-friendly Interface: Intuitive design for easy navigation.",
-      "Blog & Guide : Share articles, news, and educational content within the school community,And Access helpful resources and guides on various topics."
+      "Blog & Guide : Share articles, news, and educational content within the school community,And Access helpful resources and guides on various topics.",
     ],
   },
   {
     title: "BRIDGE",
     subTitle: "Japan Job Seeking Software",
     website: "http://www.bridge-mm.com",
-    languages: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    stack: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    languages: ["Vue", "Inertia", "Laravel", "Tailwind"],
     paragraph:
       "Bridge, our agency company, focuses on assisting teenagers in finding jobs in Japan. By simply uploading their resumes, we connect them with suitable employment opportunities, helping them kickstart their careers.",
     image: [bridge1, bridge2, bridge3, bridgelogo],
-    body: [
+    body1: [
       "Create CV: Users can easily create their own professional CVs and customization options.",
       "Code Testing: Job seekers can demonstrate their technical skills by completing coding challenges in various programming languages.",
+    ],
+    body2: [
       "CV Reviews: Users can submit their CVs for feedback and suggestions from industry professionals or peers",
       "Interview Scheduling: The app helps users manage their interview schedules by providing options to accept, decline, or reschedule interviews.",
     ],
@@ -55,92 +61,136 @@ const projects = [
     title: "SharkTopSun",
     subTitle: "Product Show Case",
     website: "http://www.sharktopsun.com",
-    languages: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    stack: "VILT (Vue, Inertia, Laravel, Tailwind)",
+    languages: ["Vue", "Inertia", "Laravel", "Tailwind"],
     paragraph:
       "SharkTopSun is a showcase website for solar panels, inverters, and batteries. It includes information about their products, details about each product, and articles providing insights into their offerings.",
     image: [shark1, shark2, shark3, sharklogo],
-    body: [
+    body1: [
       "Product Detail: Viewer can easily find products with categories and check product information details ",
       "Inquiry for Product: if Viewer want to buy or want more detail of the product viewer can send email to administrator",
       "Article or Blog Post: Viewer can also view articles of sharktopsun products likes knowledge sharing that upload from admin side",
+    ],
+    body2: [
       "Upload: Easily upload products information data, images, homepage banner and so on",
       "User-friendly Interface: Intuitive design for easy navigation.",
-      "Dashboard : admin can easily see most of the information about their website how many viewer of webpage like this"
+      "Dashboard : admin can easily see most of the information about their website how many viewer of webpage like this",
     ],
   },
   {
     title: "OFF-LOG",
     subTitle: "HR Management System",
     website: "http://www.sharktopsun.com",
-    languages: "MERN (Mongo, Express, React, Node)",
+    stack: "MERN (Mongo, Express, React, Node)",
+    languages: ["Mongo", "Express", "React", "Node"],
     paragraph:
       "Off-log is a comprehensive online platform that automates HR processes, enhances communication, and streamlines employee management. Key features include:",
     image: [offlog1, offlog2, offlog3, offloglogo],
-    body: [
+    body1: [
       "User Authentication and Access Control: Secure login with multiple user roles.",
       "Leave and Attendance Management: Online leave requests, approval, and tracking.",
       "Reporting and Analytics: Customizable reports for HR metrics and data-driven insights.",
+    ],
+    body2: [
       "Customize Holidays Calendar: Capability to customize and manage holidays and time-off policies according to the organization's requirements.",
       "Payroll Management: Comprehensive payroll processing and management, including salary calculations, tax deductions, and generating pay stubs.",
     ],
-  }
+  },
 ];
 
 const Project = () => {
+  const [projectData, setProjectData] = useState();
+  const [showModal, setShowModal] = useState(false);
+
+  const modalShowHandler = (index) => {
+    toggleModal();
+    setProjectData((prev) => (prev = projects[index]));
+  };
+
+  const toggleModal = () => {
+    setShowModal((prev) => !prev);
+  };
   return (
-    <section className="w-full bg-black text-white py-10 px-32">
+    <section className="w-full bg-black text-white">
       {/* Start Header */}
-      <div className="text-center pt-14">
-        <p className="text-[40px] font-bold mb-3">
+      <div className="text-center pt-14 mb-14">
+        <p className="text-[40px] font-bold mb-4">
           My <span className="text-red-500">Projects</span>
         </p>
+        <p className="font-bold">Some Things I've Build</p>
       </div>
       {/* End Header */}
 
-      <p className="font-bold my-10">Some Things I've Build</p>
+      <div className="grid grid-cols-12">
+        <div className="col-start-2 col-span-11 mr-20">
+          {projects.map((project, index) => (
+            <>
+              {index % 2 === 0 ? (
+                <div
+                  className="grid grid-cols-12 mb-8 place-items-center cursor-pointer"
+                  key={index}
+                  onClick={() => modalShowHandler(index)}
+                >
+                  <div className="col-start-2 col-span-5">
+                    <p className="text-red-500 tracking-wide">
+                      Featured Project
+                    </p>
+                    <p className="text-lg tracking-wide my-3">
+                      {project.title}
+                    </p>
+                    <p className="bg-[#151414] rounded relative z-10 py-4 px-3 mb-2">
+                      {project.paragraph}
+                    </p>
+                    <ul>
+                      {project.languages.map((language) => (
+                        <li className="inline-block mr-4 bg-[#DF4F4F] px-3 rounded-md">
+                          {language}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
 
-      {projects.map((project, index) => (
-        <>
-          {index % 2 === 0 ? (
-            <div className="grid grid-cols-2 mb-8" key={index}>
-              <div className="col-span-1">
-                <p className="text-red-500 tracking-wide">Featured Project</p>
-                <p className="text-lg tracking-wide my-3">{project.title}</p>
-                <p className="bg-zinc-600 rounded relative z-10 py-4 px-3 mb-2">
-                  {project.paragraph}
-                </p>
-                              
-              </div>
+                  <div className="col-span-6 -translate-x-1/4 relative z-0 hover:z-10">
+                    <div className="w-full h-full bg-black/50 hover:bg-transparent absolute left-0"></div>
+                    <img src={project.image[0]} alt="" />
+                  </div>
+                </div>
+              ) : (
+                <div
+                  className="grid grid-cols-12 mb-8 place-items-center cursor-pointer"
+                  key={index}
+                  onClick={() => modalShowHandler(index)}
+                >
+                  <div className="col-start-1 col-span-6 translate-x-1/4 relative z-0 hover:z-20">
+                    <div className="w-full h-full bg-black/50 hover:bg-transparent absolute left-0"></div>
+                    <img src={project.image[0]} alt="" />
+                  </div>
 
-              <div className="col-span-1 -translate-x-1/4 relative z-0">
-                <div className="w-full h-full bg-red-700/40 absolute left-0"></div>
-                <img src={project.image[0]} alt={project.image[0]} />
-              </div>
-            </div>
-          ) : (
-            <div className="grid grid-cols-2 mb-8" key={index}>
-              <div className="col-span-1 translate-x-1/4 relative z-0">
-                <div className="w-full h-full bg-red-700/40 absolute left-0"></div>
-                <img src={project.image[0]} alt={project.image[0]} />
-              </div>
-
-              <div className="col-span-1 text-right">
-                <p className="text-red-500 tracking-wide">Featured Project</p>
-                <p className="text-lg tracking-wide my-3">{project.title}</p>
-                <p className="bg-zinc-600 rounded relative z-10 py-4 px-3 mb-2">
-                  {project.paragraph}
-                </p>
-                {/* <ul>
-                                            {
-                                                project.languages.map(language =><li className='inline-block mr-8'>{language}</li>)
-                                            }
-                                        </ul>                             */}
-              </div>
-            </div>
-          )}
-        </>
-      ))}
-      {/* <ProjectDetailModal /> */}
+                  <div className="col-span-5 text-right">
+                    <p className="text-red-500 tracking-wide">
+                      Featured Project
+                    </p>
+                    <p className="text-lg tracking-wide my-3">
+                      {project.title}
+                    </p>
+                    <p className="bg-[#1D1C1C] rounded relative z-10 py-4 px-3 mb-2">
+                      {project.paragraph}
+                    </p>
+                    <ul>
+                      {project.languages.map((language) => (
+                        <li className="inline-block mr-8 bg-[#DF4F4F] px-3 rounded-md">
+                          {language}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </>
+          ))}
+        </div>
+      </div>
+      {showModal && <ProjectDetailModal projectData={projectData} close={toggleModal} />}
     </section>
   );
 };
